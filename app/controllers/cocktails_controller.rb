7ff71f02f044686be76df4860903565a2b590857
masoud arabi <arabimasoud88@gmail.com>
2020-11-12 16:35:24 -0500

    tha last update for today

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,13 +5,26 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @ingredients = Ingredient.all
+    @doses = @cocktail.doses
   end
 
   def new
     @cocktail = Cocktail.new
-    @ingredient = Ingredient.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:dose).permit(:description)
   end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,27 @@
 class DosesController < ApplicationController
+   def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @doses = @cocktail.doses
+      render "cocktails/show"
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+    def new
+      @dose = Dose.new
+    end
+
+    def destroy
+      @dose.destroy
+      redirect_to cocktails_path
+    end
 end
