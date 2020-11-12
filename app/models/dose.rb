d18c5779c999e9f35ce1de73e2760662e15c4b28
masoud arabi <arabimasoud88@gmail.com>
2020-11-12 13:56:13 -0500

    generated models and validations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :description, :cocktail_id, :ingredient_id, presence: true, uniqueness: true
-
+  validates :description, presence: true
+  validates :cocktail_id, :ingredient_id, uniqueness: {
+    scope: [:cocktail_id, :ingredient_id]
+  }
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,4 @@
 class Ingredient < ApplicationRecord
-  has_many :doses, dependent: :destroy
-  validates :name, presence: true
+  has_many :doses
+  validates :name, presence: true, uniqueness: true
 end
